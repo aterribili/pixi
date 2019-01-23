a1c29a5e29ba5c8d2a4b1d9ba96d2d1e8cd3e286
Caio Tavares <caio.tavares11@gmail.com>
2019-01-23 19:30:33 -0200

    tetra

--- resources/index.js
+++ resources/index.js
@@ -156,7 +156,7 @@ function setupMovements() {
   };
 
   right.press = () => {
-    explorer.vx = 5;
+    explorer.vx = 0.5;
     explorer.vy = 0;
   };
   right.release = () => {
@@ -191,6 +191,9 @@ function play(delta) {
   explorer.x += explorer.vx;
   explorer.y += explorer.vy;
   blobLoop(delta);
+  if (hitTestRectangle(explorer, treasure)) {
+    console.log('XABLON');
+  }
 }
 
 function gameLoop(delta) {
@@ -203,6 +206,55 @@ function setup() {
   initializeVelocities();
   setupMovements();
   addBlobs();
+
   state = play;
   app.ticker.add(d => gameLoop(d));
 }
+
+function hitTestRectangle(r1, r2) {
+
+  //Define the variables we'll need to calculate
+  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+  //hit will determine whether there's a collision
+  hit = false;
+
+  //Find the center points of each sprite
+  r1.centerX = r1.x + r1.width / 2;
+  r1.centerY = r1.y + r1.height / 2;
+  r2.centerX = r2.x + r2.width / 2;
+  r2.centerY = r2.y + r2.height / 2;
+
+  //Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+
+  //Calculate the distance vector between the sprites
+  vx = r1.centerX - r2.centerX;
+  vy = r1.centerY - r2.centerY;
+
+  //Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+  //Check for a collision on the x axis
+  if (Math.abs(vx) < combinedHalfWidths) {
+
+    //A collision might be occurring. Check for a collision on the y axis
+    if (Math.abs(vy) < combinedHalfHeights) {
+
+      //There's definitely a collision happening
+      hit = true;
+    } else {
+      hit = false;
+    }
+  } else {
+    hit = false;
+  }
+
+  //`hit` will be either `true` or `false`
+  return hit;
+};
+
